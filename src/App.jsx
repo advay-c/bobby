557b2b9fca47b51bd6109ba7b9a5eed93bc5cddb
wuxxy <33169104+wuxxy@users.noreply.github.com>
2024-01-05 22:56:35 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const customStyles = {
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
     background: 'transparent',
-    border: 0
+    border: 0,
   },
   overlay:{
     background: "rgba(0,0,0,40%)"
@@ -19,6 +19,7 @@ const customStyles = {
 };
 function App() {
   const [firstSection, setFirstSection] = useState(false)
+  const [secondSection, setSecondSection] = useState(false)
   return (
     <>
       <div className='bg-background g p-4 min-h-screen flex flex-col items-center'>
@@ -26,11 +27,7 @@ function App() {
           <div className='title-and-description relative text-left w-3/4'>
             <h1 className='text-5xl font-bold'>{config.title}</h1>
             <h3>{config.description['description-text']}</h3>
-            <div className='flex flex-row gap-2 justify-center mt-5'>
-              <img className='object-cover rounded-sm w-[350px] h-[200px] ' src={config.description['img-1']}/>
-              <img className='object-cover rounded-sm w-[350px] h-[200px]' src={config.description['img-2']}/>
-            </div>
-            <div className='relative mt-5'>
+            <div className='relative mt-[12.5%]'>
               <button className='self-center  bg-brnd-400 absolute translate-x-[-50%] left-[50%] px-16 py-4 text-lg text-brnd-100' onClick={() => document.getElementById(config.sections[0].name).scrollIntoView({behavior:"smooth"})}>Next</button>
             </div>
           </div>
@@ -50,17 +47,29 @@ function App() {
               contentLabel={config.sections[0].name}
               overlay={"bg-brnd-700"}
               shouldFocusAfterRender={true}
-              
+              onAfterOpen={() => {
+                document.body.style.top = `-${window.scrollY}px`
+                document.body.style.position = 'fixed'
+              }}
+              onAfterClose={() => {
+                const scrollY = document.body.style.top
+                document.body.style.position = ''
+                document.body.style.top = ''
+                window.scrollTo(0, parseInt(scrollY || '0') * -1)
+              }}
             >
-              <div className='w-[80vw] bg-gray-900 rounded-md p-4'>
+              <div className='w-[80vw] bg-gray-900 rounded-md p-4 overflow-x-hidden max-h-screen'>
                 
-                <ol class="relative border-s border-gray-200 dark:border-gray-700 flex justify-center flex-col h-full" >                  
+                <ol className="relative border-s border-gray-200 dark:border-gray-700 overflow-scroll flex justify-center flex-col" >                  
                   <h3 className='text-4xl font-bold text-center p-2 text-brnd-200 mb-auto'>{config.sections[0].name}</h3>
-                    <li class="ms-4 mb-4">
+                    <li className="ms-4 mb-4 text-gray-300">
                       <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                      <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">June 2022</time>
-                      <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Certified Lifeguard</h3>
-                      <img className='w-4/12' src="https://njswim.com/wp-content/uploads/Lifeguard-at-Pool.jpg" />
+                      <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">October 2023 - June 2024</time>
+                      <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Recess Islington</h3>
+                      <p>
+                        Volunteer my time at my local church to aid children with special needs and look after/take care of them. This event is once a month from 4-9PM.
+                      </p>
+                      <img className='w-4/12' src="https://www.umc.org/-/media/umc-media/2021/08/18/19/57/ask-the-umc-local-church-1152x762px.jpg?h=762&iar=0&mh=768&w=1152&hash=4A40677B4FE759444A1260464586B87C" />
                       {/* Certified Lifeguard
                       8 Years experience Taekwondo (Black Belt)
                       Chess prodigy
@@ -68,15 +77,21 @@ function App() {
                       Volunteered with special needs children
                       Taekwondo Instructor */}
                     </li>
-                    <li class="mb-4 ms-4">
-                        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">8 Years</time>
-                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Taekwando ( Black Belt )</h3>
+                    <li class="mb-4 ms-4 text-gray-300">
+                        <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">September 2022 to December 2022</time>
+                        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">Taekwando Instructor Volunteer </h3>
+                        <p>Dedicated my time to training kids skills with sparring, and patterning, whilst also preparing them for provincial tournaments.</p>
                         <img className='w-4/12' src="https://www.balletcentredubai.com/wp-content/uploads/2021/10/SIZE-A2-6-scaled.jpg" />
                     </li>
-                    <li class="ms-4 mb-auto">
-                        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <h3 class="text-xl font-semibold text-gray-900 dark:text-yellow-400">Distinction award in Gauss Contest</h3>
+                    <li class="ms-4 mb-auto text-gray-200">
+                        <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">March 2023 to May 2023</time>
+                        
+                        <h3 className="text-xl font-semibold text-gray-900 dark:text-gray-300">Community Center Youth Assistance Volunteer</h3>
+                        <p>Aimed to spread awareness and encourage the youth to participate in more events at the local community center</p>
+                        <img className='w-4/12' src="https://sportsfacilities.com/wp-content/uploads/2018/07/Community-center-lobby-scaled.jpg" />
+
                     </li>
                 </ol>
 
@@ -92,38 +107,54 @@ function App() {
             </h4>
           </div>
           <div className="section-images w-2/5 flex flex-col items-center justify-center">
-            <button onClick={() => setFirstSection(true)} className='bg-yellow-500 px-8 py-3 font-bold text-yellow-300 uppercase rounded-sm'>Open</button>
+            <button onClick={() => setSecondSection(true)} className='bg-yellow-500 px-8 py-3 font-bold text-yellow-300 uppercase rounded-sm'>Open</button>
             <Modal
-              isOpen={firstSection}
-              onRequestClose={() => setFirstSection(false)}
+              isOpen={secondSection}
+              onRequestClose={() => setSecondSection(false)}
               style={customStyles}
               contentLabel={config.sections[1].name}
               overlay={"bg-brnd-700"}
               shouldFocusAfterRender={true}
+              onAfterOpen={() => {
+                document.body.style.top = `-${window.scrollY}px`
+                document.body.style.position = 'fixed'
+              }}
+              onAfterClose={() => {
+                const scrollY = document.body.style.top
+                document.body.style.position = ''
+                document.body.style.top = ''
+                window.scrollTo(0, parseInt(scrollY || '0') * -1)
+              }}
             >
-              <div className='w-[80vw] bg-gray-900 rounded-md p-4'>
-                
-                <ol class="relative border-s border-gray-200 dark:border-gray-700 flex justify-center flex-col h-full" >                  
-                  <h3 className='text-4xl font-bold text-center p-2 text-brnd-200 mb-auto'>{config.sections[1].name}</h3>
-                    <li class="ms-4 mb-4">
-                      <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                      <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">June 2022</time>
-                      <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                        PUT SOME ACADEMIC ACHIEVEMENT HERE PLEASEE
-                      </h3>
-                    </li>
-                    <li class="mb-4 ms-4">
-                        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">8 Years</time>
-                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Taekwando ( Black Belt )</h3>
-                    </li>
-                    <li class="ms-4 mb-auto">
-                        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <h3 class="text-xl font-semibold text-gray-900 dark:text-yellow-400">
-                          PUT SOME ACADEMIC ACHIEVEMENT HERE PLEASEE
-                        </h3>
-                    </li>
-                </ol>
+              <div className='w-[80vw] bg-gray-900 rounded-md p-4 max-h-screen flex flex-col text-gray-300 gap-2 text-xl'>
+                <div>
+
+                  Distinction Award in Waterloo’s Mathametic Gauss Contest
+                  2022
+                </div>
+
+                <div>
+
+                Caravan Short Storry Contestant
+                Written a short story that was published in a book with a collection of young canadians creative novels (2019)
+                </div>
+
+                <div>
+
+                Glenforest Secondary School Academic Excellence Award
+                Acheived an overall average of 85%+
+                </div>
+
+                <div>
+
+                Chess Prodigy
+                Published on local frontpage news article The guardian - Etobicoke
+                </div>
+
+                <div>
+
+                Captain of Jr Boys Basketball Team 
+                </div>
 
 
               </div>
@@ -133,6 +164,15 @@ function App() {
         <section className="h-screen flex flex-col items-center justify-center">
           <h2 className="text-6xl text-white">This Was my Ted-Talk</h2>
           <h4 className="text-3xl text-gray-400">Thank You!</h4>
+          <a className='bg-gray-900 p-2' href="https://instagram.com/bobbyf0814">
+            <span style={{fontSize: 40}}>
+                <span style={{color: "red"}}>
+                    <i className="fab fa-instagram mr-2"></i>
+                    Follow me on Instagram!
+                </span>
+            </span>
+          </a>
+
         </section>
       </div>
     </>
